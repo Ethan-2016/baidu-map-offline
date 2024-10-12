a25e3b6fbc5fa60075e0d79552822306a8d7c01a
吴应 <ethan-2016@outlook.com>
2024-10-12 13:55:28 +0800

    组件名称

--- components/index.js
+++ components/index.js
@@ -1,98 +1,95 @@
 import BaiduMapOffline from './map/Map.vue'
-import BmView from './map/MapView.vue'
-import BmScale from './controls/Scale.vue'
-import BmNavigation from './controls/Navigation.vue'
-import BmMapType from './controls/MapType.vue'
-import BmOverviewMap from './controls/OverviewMap.vue'
-import BmGeolocation from './controls/Geolocation.vue'
-import BmCopyright from './controls/Copyright.vue'
-import BmCityList from './controls/CityList.vue'
-import BmPanorama from './controls/Panorama.vue'
-import BmControl from './controls/Control.vue'
-import BmMarker from './overlays/Marker.vue'
-import BmPointCollection from './overlays/PointCollection.vue'
-import BmPolyline from './overlays/Polyline.vue'
-import BmPolygon from './overlays/Polygon.vue'
-import BmCircle from './overlays/Circle.vue'
-import BmGround from './overlays/Ground.vue'
-import BmLabel from './overlays/Label.vue'
-import BmInfoWindow from './overlays/InfoWindow.vue'
-import BmOverlay from './overlays/Overlay.vue'
-import BmContextMenu from './context-menu/Menu.vue'
-import BmContextMenuItem from './context-menu/Item.vue'
-import BmLocalSearch from './search/LocalSearch.vue'
-import BmTransit from './search/Transit.vue'
-import BmWalking from './search/Walking.vue'
-import BmDriving from './search/Driving.vue'
-import BmBus from './search/Bus.vue'
-import BmTile from './layers/Tile.vue'
-import BmTraffic from './layers/Traffic.vue'
-import BmBoundary from './others/Boundary.vue'
-import BmAutoComplete from './others/AutoComplete.vue'
+import BmOfflineView from './map/MapView.vue'
+import BmOfflineScale from './controls/Scale.vue'
+import BmOfflineNavigation from './controls/Navigation.vue'
+import BmOfflineMapType from './controls/MapType.vue'
+import BmOfflineOverviewMap from './controls/OverviewMap.vue'
+import BmOfflineGeolocation from './controls/Geolocation.vue'
+import BmOfflineCopyright from './controls/Copyright.vue'
+import BmOfflineCityList from './controls/CityList.vue'
+import BmOfflinePanorama from './controls/Panorama.vue'
+import BmOfflineControl from './controls/Control.vue'
+import BmOfflineMarker from './overlays/Marker.vue'
+import BmOfflinePointCollection from './overlays/PointCollection.vue'
+import BmOfflinePolyline from './overlays/Polyline.vue'
+import BmOfflinePolygon from './overlays/Polygon.vue'
+import BmOfflineCircle from './overlays/Circle.vue'
+import BmOfflineGround from './overlays/Ground.vue'
+import BmOfflineLabel from './overlays/Label.vue'
+import BmOfflineInfoWindow from './overlays/InfoWindow.vue'
+import BmOfflineOverlay from './overlays/Overlay.vue'
+import BmOfflineContextMenu from './context-menu/Menu.vue'
+import BmOfflineContextMenuItem from './context-menu/Item.vue'
+import BmOfflineLocalSearch from './search/LocalSearch.vue'
+import BmOfflineTransit from './search/Transit.vue'
+import BmOfflineWalking from './search/Walking.vue'
+import BmOfflineDriving from './search/Driving.vue'
+import BmOfflineBus from './search/Bus.vue'
+import BmOfflineTile from './layers/Tile.vue'
+import BmOfflineTraffic from './layers/Traffic.vue'
+import BmOfflineBoundary from './others/Boundary.vue'
+import BmOfflineAutoComplete from './others/AutoComplete.vue'
 
-import BmlMarkerClusterer from './extra/MarkerClusterer.vue'
-import BmlLushu from './extra/Lushu.vue'
-import BmlHeatmap from './extra/Heatmap.vue'
-import BmlCurveLine from './extra/CurveLine.vue'
+import BmOfflinelMarkerClusterer from './extra/MarkerClusterer.vue'
+import BmOfflinelLushu from './extra/Lushu.vue'
+import BmOfflinelHeatmap from './extra/Heatmap.vue'
+import BmOfflinelCurveLine from './extra/CurveLine.vue'
 
 export default {
   install (Vue, options) {
-    const {ak} = options
-    Vue.prototype._BMapOffline = () => ({ak})
-
     Vue.component('baidu-map-offline', BaiduMapOffline)
-    Vue.component('bm-offline-view', BmView)
+    Vue.component('bm-offline-view', BmOfflineView)
 
-    Vue.component('bm-offline-scale', BmScale)
-    Vue.component('bm-offline-navigation', BmNavigation)
-    Vue.component('bm-offline-map-type', BmMapType)
-    Vue.component('bm-offline-overview-map', BmOverviewMap)
-    Vue.component('bm-offline-geolocation', BmGeolocation)
-    Vue.component('bm-offline-copyright', BmCopyright)
-    Vue.component('bm-offline-city-list', BmCityList)
-    Vue.component('bm-offline-panorama', BmPanorama)
-    Vue.component('bm-offline-control', BmControl)
+    Vue.component('bm-offline-scale', BmOfflineScale)
+    Vue.component('bm-offline-navigation', BmOfflineNavigation)
+    Vue.component('bm-offline-map-type', BmOfflineMapType)
+    Vue.component('bm-offline-overview-map', BmOfflineOverviewMap)
+    Vue.component('bm-offline-geolocation', BmOfflineGeolocation)
+    Vue.component('bm-offline-copyright', BmOfflineCopyright)
+    Vue.component('bm-offline-city-list', BmOfflineCityList)
+    Vue.component('bm-offline-panorama', BmOfflinePanorama)
+    Vue.component('bm-offline-control', BmOfflineControl)
 
-    Vue.component('bm-offline-marker', BmMarker)
-    Vue.component('bm-offline-point-collection', BmPointCollection)
-    Vue.component('bm-offline-polyline', BmPolyline)
-    Vue.component('bm-offline-polygon', BmPolygon)
-    Vue.component('bm-offline-circle', BmCircle)
-    Vue.component('bm-offline-ground', BmGround)
-    Vue.component('bm-offline-label', BmLabel)
-    Vue.component('bm-offline-info-window', BmInfoWindow)
-    Vue.component('bm-offline-overlay', BmOverlay)
+    Vue.component('bm-offline-marker', BmOfflineMarker)
+    Vue.component('bm-offline-point-collection', BmOfflinePointCollection)
+    Vue.component('bm-offline-polyline', BmOfflinePolyline)
+    Vue.component('bm-offline-polygon', BmOfflinePolygon)
+    Vue.component('bm-offline-circle', BmOfflineCircle)
+    Vue.component('bm-offline-ground', BmOfflineGround)
+    Vue.component('bm-offline-label', BmOfflineLabel)
+    Vue.component('bm-offline-info-window', BmOfflineInfoWindow)
+    Vue.component('bm-offline-overlay', BmOfflineOverlay)
 
-    Vue.component('bm-offline-context-menu', BmContextMenu)
-    Vue.component('bm-offline-context-menu-item', BmContextMenuItem)
+    Vue.component('bm-offline-context-menu', BmOfflineContextMenu)
+    Vue.component('bm-offline-context-menu-item', BmOfflineContextMenuItem)
 
-    Vue.component('bm-offline-local-search', BmLocalSearch)
-    Vue.component('bm-offline-transit', BmTransit)
-    Vue.component('bm-offline-walking', BmWalking)
-    Vue.component('bm-offline-driving', BmDriving)
-    Vue.component('bm-offline-bus', BmBus)
+    Vue.component('bm-offline-local-search', BmOfflineLocalSearch)
+    Vue.component('bm-offline-transit', BmOfflineTransit)
+    Vue.component('bm-offline-walking', BmOfflineWalking)
+    Vue.component('bm-offline-driving', BmOfflineDriving)
+    Vue.component('bm-offline-bus', BmOfflineBus)
 
-    Vue.component('bm-offline-tile', BmTile)
-    Vue.component('bm-offline-traffic', BmTraffic)
+    Vue.component('bm-offline-tile', BmOfflineTile)
+    Vue.component('bm-offline-traffic', BmOfflineTraffic)
 
-    Vue.component('bm-offline-auto-complete', BmAutoComplete)
-    Vue.component('bm-offline-boundary', BmBoundary)
+    Vue.component('bm-offline-auto-complete', BmOfflineAutoComplete)
+    Vue.component('bm-offline-boundary', BmOfflineBoundary)
   }
 }
 
 export {
-  BaiduMapOffline, BmView,
-  BmScale, BmNavigation, BmMapType, BmOverviewMap, BmGeolocation, BmCopyright, BmCityList, BmPanorama, BmControl,
-  BmMarker, BmPointCollection, BmPolyline, BmPolygon, BmCircle, BmGround, BmLabel, BmInfoWindow, BmOverlay,
-  BmContextMenu, BmContextMenuItem,
-  BmLocalSearch, BmTransit, BmWalking, BmDriving, BmBus,
-  BmTile, BmTraffic,
-  BmBoundary, BmAutoComplete
+  BaiduMapOffline, BmOfflineView,
+  BmOfflineScale, BmOfflineNavigation, BmOfflineMapType, BmOfflineOverviewMap, BmOfflineGeolocation, BmOfflineCopyright, BmOfflineCityList, BmOfflinePanorama, BmOfflineControl,
+  BmOfflineMarker, BmOfflinePointCollection, BmOfflinePolyline, BmOfflinePolygon, BmOfflineCircle, BmOfflineGround, BmOfflineLabel, BmOfflineInfoWindow, BmOfflineOverlay,
+  BmOfflineContextMenu, BmOfflineContextMenuItem,
+  BmOfflineLocalSearch, BmOfflineTransit, BmOfflineWalking, BmOfflineDriving, BmOfflineBus,
+  BmOfflineTile, BmOfflineTraffic,
+  BmOfflineBoundary, BmOfflineAutoComplete
 }
 
 export {
-  BmlMarkerClusterer,
-  BmlLushu,
-  BmlHeatmap,
-  BmlCurveLine
+  BmOfflinelMarkerClusterer,
+  BmOfflinelLushu,
+  BmOfflinelHeatmap,
+  BmOfflinelCurveLine
 }
